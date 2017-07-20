cc1379c4e09d859fc7e902b4d9de62d6fb7f70dd
Juan Diego Jimenez <jdiejim@gmail.com>
2017-07-19 21:38:58 -0600

    add redux setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import { Route } from 'react-router-dom';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const history = createHistory();
+const router = routerMiddleware(history);
+const middleware = [router, thunk];
+const store = createStore(
+  rootReducer,
+  devTools,
+  applyMiddleware(...middleware)
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
